665dcdc7dd29289add27566d82c72b354b9d03b9
Marc Hedlund <marc@precipice.org>
2016-08-06 01:02:27 -0700

    Add basic logging

--- hello.go
+++ hello.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
+func Log(handler http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+	handler.ServeHTTP(w, r)
+    })
+}
+
 func main() {
     http.Handle("/", http.FileServer(http.Dir("assets")))
 	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	if err := http.ListenAndServe(addr, Log(http.DefaultServeMux)); err != nil {
 		panic(err)
 	}
 }
