248c603dece48a62e651aa1e9fed0c7f053f08a0
Marc Hedlund <marc@precipice.org>
2016-08-06 01:03:25 -0700

    gofmt fixes

--- hello.go
+++ hello.go
@@ -8,14 +8,14 @@ import (
 )
 
 func Log(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-	handler.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("assets")))
+	http.Handle("/", http.FileServer(http.Dir("assets")))
 	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	if err := http.ListenAndServe(addr, Log(http.DefaultServeMux)); err != nil {
 		panic(err)
